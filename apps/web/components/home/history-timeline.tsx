fe632abf6f43e9e9810bdcdea112a7279f20e377
Jeffin2007 <jeffinjosva03@gmail.com>
2026-09-01 20:13:34 +0530

    fix(syntax): correct closing div tags in history-timeline component

--- apps/web/components/home/history-timeline.tsx
+++ apps/web/components/home/history-timeline.tsx
@@ -130,6 +130,7 @@ export function HistoryTimeline() {
                         </div>
                       </Card>
                     </ScrollReveal>
+                  </div>
                 </div>
               );
             })}
